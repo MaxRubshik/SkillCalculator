5a62372ee067ae474ea0a4f8a37f905ce44f61d4
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2021-02-24 16:25:17 +0300

    init

---
+++ src/main/java/com/house/skills2/Skills2Application.java
@@ -0,0 +1,13 @@
+package com.house.skills2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skills2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Skills2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/house/skills2/Skills2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.house.skills2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skills2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
